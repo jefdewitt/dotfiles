b0a7d7ca8d9688454b8ac69bb90554b8afb45f29
CJ Taylor <cj.taylor@cdinteractive.com>
2017-01-11 15:18:47 -0500

    Updated dotfiles.

---
+++ scripts/mobile/build-all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT"}
+
+tsui_dir="$DEV_PROJECTS_ROOT/tsui"
+mobile_dir="$DEV_PROJECTS_ROOT/mobile"
+
+#clear out the widget/releses folder in tsui so we can easily see what specific widgets we are working on
+echo "cleaning the tsui releases folder..."
+rm -Rf "$tsui_dir/widgets/releases/*"
+
+#copy the tsui project to the mobile project's tsuilib folder
+echo "copying tsui project to mobile's tsuilib folder..."
+cp -Rf "$tsui_dir/" "$mobile_dir/app/resources/shared/extlibs/tsuilib/"
+if cd "$mobile_dir/app/resources/shared/extlibs/tsuilib/";
+then
+    echo "removing git files from tsuilib folder to prevent submodule warnings in git"
+    rm -rf .git
+    rm -rf .gitignore
+fi
+
+#build the mobile site
+echo "Finally, building mobile..."
+cd "$mobile_dir"
+{
+  grunt release
+} || {  # a cheap try-catch, attempt a npm install automatically then re-attempt grunt
+  echo "Forcing npm install to resolve common 'grunt not found'"
+  npm install
+  grunt release
+}
