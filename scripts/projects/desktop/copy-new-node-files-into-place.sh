1d957047915009c14ae0dd83633fd340c00b5bda
Brad Ball <Brad.Ball@cdinteractive.com>
2017-01-19 15:53:45 -0500

    Added some additional paths

--- scripts/projects/desktop/copy-new-node-files-into-place.sh
+++ scripts/projects/desktop/copy-new-node-files-into-place.sh
@@ -1,17 +1,20 @@
+
 #!/bin/sh
 
 target_dir=/nas/drupal
 src_dir=/mnt/code/Drupal_Adw
 
-cp -rf \
-    $src_dir/sites/twinspires.com/themes/ts2/templates/panels/panels-pane--custom-offers-list-block.tpl.php \
-    $target_dir/sites/twinspires.com/themes/ts2/templates/panels/panels-pane--custom-offers-list-block.tpl.php
 
 mkdir -p $target_dir/sites/twinspires.com/.deployment-packages/r22.i1
+mkdir -p $target_dir/sites/twinspires.com/.deployment-packages/r22.i1/Node
 
 cp -rf \
     $src_dir/sites/twinspires.com/.deployment-packages/r22.i1/package.yaml \
     $target_dir/sites/twinspires.com/.deployment-packages/r22.i1/package.yaml
+
+cp -rf \
+    $src_dir/sites/twinspires.com/.deployment-packages/r22.i1/Node/offers.yaml \
+    $target_dir/sites/twinspires.com/.deployment-packages/r22.i1/Node/offers.yaml
 
 cp -rf \
     $src_dir/sites/all/modules/deployment_ts_account_pages/deployment_ts_account_pages.pages_default.inc \
@@ -22,6 +25,9 @@ cp -rf \
     $target_dir/sites/twinspires.com/themes/ts2/template.php
 
 cp -rf \
+    $src_dir/sites/all/modules/brisnet/css/widget.offers.css \
+    $target_dir/sites/all/modules/brisnet/css/widget.offers.css
+
+cp -rf \
     $src_dir/sites/all/postInstall/drupalAdw.xml \
     $target_dir/sites/all/postInstall/drupalAdw.xml
-
