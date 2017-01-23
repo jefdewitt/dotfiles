128cecd573516a1bd118900baf8e379c6840ddf7
CJ Taylor <cj.taylor@cdinteractive.com>
2017-01-23 14:00:29 -0500

    Added n and nvm install scripts.

---
+++ scripts/apps/n/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh 
+
+# https://github.com/tj/n#installation
+curl -L https://git.io/n-install | bash
---
+++ scripts/apps/nvm/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# https://github.com/creationix/nvm#install-script
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
