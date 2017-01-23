65c27cf702b226541b86dfce004f6297d0b99091
CJ Taylor <cj.taylor@cdinteractive.com>
2017-01-23 10:21:20 -0500

    Refactored desktop widget replacement to reuse existing scripts for other projects.

--- scripts/projects/desktop/build-and-replace-widgets.sh
+++ scripts/projects/desktop/build-and-replace-widgets.sh
@@ -13,20 +13,14 @@
 ${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT"}
 
 host_projects_root=$DEV_PROJECTS_ROOT
-tsui_dir="$DEV_PROJECTS_ROOT/tsui"
-dev_vm_dir="$DEV_PROJECTS_ROOT/dev_vm"
+project_scripts="$DEV_PROJECTS_ROOT/dotfiles/scripts/projects"
 
-vm_script_to_run="$DEV_PROJECTS_ROOT/dotfiles/scripts/projects/desktop/vm-replace-widgets.sh"
+vm_script_to_run="$project_scripts/desktop/vm-replace-widgets.sh"
 
 echo "building tsui on the host"
-cd "$tsui_dir"
-grunt
+$project_scripts/tsui/build.sh
 
-echo "giving the vm the business"
-ssh vagrant@127.0.0.1 \
-  -p 2222 -o Compression=yes -o DSAAuthentication=yes -o LogLevel=FATAL \
-  -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-  -i $dev_vm_dir/vagrant_web/.vagrant/machines/default/virtualbox/private_key \
-  "sudo -i" < "$vm_script_to_run"
+echo "Replacing widgets on the vm"
+$project_scripts/dev_vm/dev-vm-ssh-and-run-script.sh $vm_script_to_run
 
-echo "Ding! Fries are done!"
+echo "All done, you should now see your changes on local desktop site!"
