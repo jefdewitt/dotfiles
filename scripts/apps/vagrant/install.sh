ff8d3c9990b8e264f8f96a2174162b119498ed83
CJ Taylor <cj.taylor@cdinteractive.com>
2017-01-23 18:11:50 -0500

    Stubbed out vagrant and virtualbox

---
+++ scripts/apps/vagrant/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# https://releases.hashicorp.com/vagrant/1.8.5/vagrant_1.8.5.dmg
+
+# todo: script this installation out 
---
+++ scripts/apps/virtualbox/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh 
+
+# http://download.virtualbox.org/virtualbox/5.0.26/VirtualBox-5.0.26-108824-OSX.dmg
+# todo: script this installation out 
