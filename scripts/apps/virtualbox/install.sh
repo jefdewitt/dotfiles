3a2b74836c2bdd5eb4f4679ed28bce83d6dd48f5
CJ Taylor <cj.taylor@cdinteractive.com>
2017-01-24 11:20:27 -0500

    Scripted virtualbox install.

--- scripts/apps/virtualbox/install.sh
+++ scripts/apps/virtualbox/install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh 
 
 # http://download.virtualbox.org/virtualbox/5.0.26/VirtualBox-5.0.26-108824-OSX.dmg
-# todo: script this installation out 
+
+${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT"}
+
+scripts_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts"
+
+$scripts_directory/helpers/install-dmg-from-command-line.sh http://download.virtualbox.org/virtualbox/5.0.26/VirtualBox-5.0.26-108824-OSX.dmg VirtualBox
