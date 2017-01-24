774eb9230f94edad17d94fc36c3429f8624e4661
CJ Taylor <cj.taylor@cdinteractive.com>
2017-01-24 11:44:50 -0500

    Make project dir if it doesn't exist.

--- scripts/workflows/new-computer/setup.sh
+++ scripts/workflows/new-computer/setup.sh
@@ -6,6 +6,9 @@ projects_directory="$DEV_PROJECTS_ROOT"
 scripts_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts"
 workflows_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts/workflows"
 
+# make our projects directory if it doesn't exist 
+mkdir -p $projects_directory
+
 # install development tooling 
 $scripts_directory/apps/homebrew/install.sh
 $scripts_directory/apps/intellij-idea/install.sh
