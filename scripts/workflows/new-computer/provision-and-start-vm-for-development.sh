8194a2b5613f90794abea5a3781b3dd12fa37d09
CJ Taylor <cj.taylor@cdinteractive.com>
2017-01-24 11:43:18 -0500

    Fixed vagrant provisioning typo.

--- scripts/workflows/new-computer/provision-and-start-vm-for-development.sh
+++ scripts/workflows/new-computer/provision-and-start-vm-for-development.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#!/bin/sh
-
 ${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT"}
 
 scripts_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts"
@@ -17,8 +15,8 @@ cp -Rf "$initial_file" "$initial_file-$now"
 
 cp -f "$replacement_file" "$initial_file"
 
-mkdir -p "$projects_root/mobile/dist"
-mkdir -p "$projects_root/tux/dist"
+mkdir -p "$DEV_PROJECTS_ROOT/mobile/dist"
+mkdir -p "$DEV_PROJECTS_ROOT/tux/dist"
 
 cd "$dev_vm_web_dir"
 CODE_LOCATION=$DEV_PROJECTS_ROOT vagrant up
