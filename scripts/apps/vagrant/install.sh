cf5882bf3a9909a9464884d4ef8b4c363af425c6
CJ Taylor <cj.taylor@cdinteractive.com>
2017-01-24 11:20:37 -0500

    Scripted vagrant install.

--- scripts/apps/vagrant/install.sh
+++ scripts/apps/vagrant/install.sh
@@ -2,4 +2,8 @@
 
 # https://releases.hashicorp.com/vagrant/1.8.5/vagrant_1.8.5.dmg
 
-# todo: script this installation out 
+${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT"}
+
+scripts_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts"
+
+$scripts_directory/helpers/install-dmg-from-command-line.sh https://releases.hashicorp.com/vagrant/1.8.5/vagrant_1.8.5.dmg Vagrant
