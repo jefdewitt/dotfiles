57a5c923f67c48c8b13fb2dd65a1806bb3445ca1
CJ Taylor <cj.taylor@cdinteractive.com>
2017-02-08 16:58:25 -0500

    Fixed dev_vm tear down.

--- scripts/projects/dev_vm/tear-down.sh
+++ scripts/projects/dev_vm/tear-down.sh
@@ -3,7 +3,7 @@
 ${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT to local projects root directory."}
 dev_vm_dir="${VM_PROJECT_LOCATION:-$DEV_PROJECTS_ROOT/dev_vm}"
 
-if cd "$dev_vm_dir";
+if cd "$dev_vm_dir/vagrant_web";
 then 
     vagrant destroy --force
 fi
