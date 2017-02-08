e23709f61af38c124becf0a4613fc8e02e0cfdf9
CJ Taylor <cj.taylor@cdinteractive.com>
2017-02-08 17:00:09 -0500

    Updated vm bootrapping scripts.

--- scripts/workflows/new-computer/provision-and-start-vm-for-development.sh
+++ scripts/workflows/new-computer/provision-and-start-vm-for-development.sh
@@ -3,11 +3,11 @@
 ${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT"}
 
 scripts_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts"
-dev_vm_directory="${VM_PROJECT_LOCATION:-$DEV_PROJECTS_ROOT/dev_vm}"
+dev_vm_directory="${VM_PROJECT_LOCATION:-$DEV_PROJECTS_ROOT/dev_vm}/vagrant_web"
 
 now=$(date +"%Y_%m_%d_%S") # ex: 2017_01_05_44
 
-initial_file="$dev_vm_directory/vagrant_web/Vagrantfile"
+initial_file="$dev_vm_directory/Vagrantfile"
 replacement_file="$scripts_directory/projects/dev_vm/Vagrantfile-mobile-dev"
 
 # cheap way of persisting previous version of vagrantfile (better than blowing it away completely)
@@ -18,5 +18,5 @@ cp -f "$replacement_file" "$initial_file"
 mkdir -p "$DEV_PROJECTS_ROOT/mobile/dist"
 mkdir -p "$DEV_PROJECTS_ROOT/tux/dist"
 
-cd "$dev_vm_web_dir"
+cd "$dev_vm_directory"
 CODE_LOCATION=$DEV_PROJECTS_ROOT vagrant up
