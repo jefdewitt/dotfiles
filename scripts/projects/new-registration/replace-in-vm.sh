01004f33bcadf141e48aa903993cf5787006353e
CJ Taylor <cj.taylor@cdinteractive.com>
2017-03-30 09:55:43 -0400

    Cleanup of desktop new-reg helper script.

--- scripts/projects/new-registration/replace-in-vm.sh
+++ scripts/projects/new-registration/replace-in-vm.sh
@@ -4,8 +4,12 @@ ${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT"}
 
 project_scripts="$DEV_PROJECTS_ROOT/dotfiles/scripts/projects"
 
+code_location='/mnt/code/cdux-registration/dist/'
+http_location='/nas/whiteLabel/webroot/php/newreg/'
+https_location='/nas/whiteLabel/webroot-sec/php/newreg/'
+
 # copy dist to http location / change perms / change owner 
-$project_scripts/dev_vm/dev-vm-ssh-and-run-command.sh "sudo -i bash -c 'yes | cp -Rf /mnt/code/cdux-registration/dist/* /nas/whiteLabel/webroot/php/newreg/ && chmod 755 -R /nas/whiteLabel/webroot/php/newreg/* && chown netserve.common -R /nas/whiteLabel/webroot/php/newreg/*'"
+$project_scripts/dev_vm/dev-vm-ssh-and-run-command.sh "sudo -i bash -c 'yes | cp -Rf $code_location $http_location && chmod 755 -R $http_location* && chown netserve.common -R $http_location* && ls -ltr $http_location'"
 
 # copy dist to https location / change perms / change owner 
-$project_scripts/dev_vm/dev-vm-ssh-and-run-command.sh "sudo -i bash -c 'yes | cp -Rf /mnt/code/cdux-registration/dist/* /nas/whiteLabel/webroot-sec/php/newreg/ && chmod 755 -R /nas/whiteLabel/webroot-sec/php/newreg/* && chown netserve.common -R /nas/whiteLabel/webroot-sec/php/newreg/*'"
+$project_scripts/dev_vm/dev-vm-ssh-and-run-command.sh "sudo -i bash -c 'yes | cp -Rf $code_location $https_location && chmod 755 -R $https_location* && chown netserve.common -R $https_location* && ls -ltr $https_location'"
