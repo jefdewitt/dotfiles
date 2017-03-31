6230a199918a29e4f3c573016624a9ec662ee946
CJ Taylor <cj.taylor@cdinteractive.com>
2017-03-31 11:48:45 -0400

    Updated cdux-registration vm helper to mkdir if not exist.

--- scripts/projects/new-registration/replace-in-vm.sh
+++ scripts/projects/new-registration/replace-in-vm.sh
@@ -9,7 +9,7 @@ http_location='/nas/whiteLabel/webroot/php/newreg/'
 https_location='/nas/whiteLabel/webroot-sec/php/newreg/'
 
 # remove contents / copy dist to http location / change perms / change owner 
-$project_scripts/dev_vm/dev-vm-ssh-and-run-command.sh "sudo -i bash -c 'rm -Rf $http_location* && cp -Rf $code_location* $http_location && chmod 755 -R $http_location* && chown netserve.common -R $http_location* && ls -ltr $http_location'"
+$project_scripts/dev_vm/dev-vm-ssh-and-run-command.sh "sudo -i bash -c 'rm -Rf $http_location && mkdir -p $http_location && cp -Rf $code_location* $http_location && chmod 755 -R $http_location* && chown netserve.common -R $http_location* && ls -ltr $http_location'"
 
 # remove contents / copy dist to https location / change perms / change owner 
-$project_scripts/dev_vm/dev-vm-ssh-and-run-command.sh "sudo -i bash -c 'rm -Rf $https_location* && cp -Rf $code_location* $https_location && chmod 755 -R $https_location* && chown netserve.common -R $https_location* && ls -ltr $https_location'"
+$project_scripts/dev_vm/dev-vm-ssh-and-run-command.sh "sudo -i bash -c 'rm -Rf $https_location && mkdir -p $https_location && cp -Rf $code_location* $https_location && chmod 755 -R $https_location* && chown netserve.common -R $https_location* && ls -ltr $https_location'"
