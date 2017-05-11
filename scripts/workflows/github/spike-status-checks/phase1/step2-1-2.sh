cada59be902dde702ce91c2b7bc963945428d089
CJ Taylor <cj.taylor@cdinteractive.com>
2017-05-11 17:00:01 -0400

    Github-Status spike, step2-1-2.sh script.

---
+++ scripts/workflows/github/spike-status-checks/phase1/step2-1-2.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# 2.1.2 Build: Failure, Test: Pending, Approval: Pending
+
+org="cj-taylor"
+repo="tsui"
+commit="be256fb693a53b575d0361c05d8112f56026f356"
+target_url="https://github.cdinteractive.com/cj-taylor/tsui/issues/1"
+
+../add-status-to-commit.sh $org $repo $commit "Build" "failure" "Build: Failed" $target_url
+../add-status-to-commit.sh $org $repo $commit "Test" "pending" "Unit-Test: pending" $target_url
