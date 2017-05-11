7b9b05d46018eeb1cf54894a93266b8ee0b3de3e
CJ Taylor <cj.taylor@cdinteractive.com>
2017-05-11 18:19:50 -0400

    Added images of steps.

--- scripts/workflows/github/spike-status-checks/phase1/step2-2-1.sh
+++ scripts/workflows/github/spike-status-checks/phase1/step2-2-1.sh
@@ -5,7 +5,7 @@
 org="cj-taylor"
 repo="tsui"
 commit="39c3106c1427b86e9482c61c7607f34e3e363abe"
-target_url="https://github.cdinteractive.com/cj-taylor/tsui/issues/1"
+target_url="https://github.cdinteractive.com/cj-taylor/tsui/issues/2"
 
 ../add-status-to-commit.sh $org $repo $commit "Build" "success" "Build tool" $target_url
 ../add-status-to-commit.sh $org $repo $commit "Test" "failure" "Build tool" $target_url
--- scripts/workflows/github/spike-status-checks/phase1/step2-3-1.sh
+++ scripts/workflows/github/spike-status-checks/phase1/step2-3-1.sh
@@ -5,7 +5,7 @@
 org="cj-taylor"
 repo="tsui"
 commit="b149e65b427b96267ace920ba8b6552e6449860b"
-target_url="https://github.cdinteractive.com/cj-taylor/tsui/issues/1"
+target_url="https://github.cdinteractive.com/cj-taylor/tsui/issues/3"
 
 ../add-status-to-commit.sh $org $repo $commit "Build" "success" "Build tool" $target_url
 ../add-status-to-commit.sh $org $repo $commit "Test" "success" "Build tool" $target_url
