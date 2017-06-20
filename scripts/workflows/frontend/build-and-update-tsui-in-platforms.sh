4657deaa05e43d8a91282db4a9c292db54413326
CJ Taylor <cj.taylor@cdinteractive.com>
2017-06-20 11:39:48 -0400

    Updated workflows/frontend.

--- scripts/workflows/frontend/build-and-update-tsui-in-platforms.sh
+++ scripts/workflows/frontend/build-and-update-tsui-in-platforms.sh
@@ -5,15 +5,15 @@ ${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT"}
 scripts_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts/projects"
 
 # build widget in tsui
-$scripts_directory/tsui/build.sh
+"$scripts_directory"/tsui/build.sh
 
 # replace in mobile 
-    $scripts_directory/tsui/copy-to-mobile.sh   
-    $scripts_directory/mobile/build.sh
+"$scripts_directory"/tsui/copy-to-mobile.sh   
+"$scripts_directory"/mobile/build.sh
 
 # replace in tux 
-    $scripts_directory/tsui/copy-to-tux.sh
-    $scripts_directory/tux/build.sh
+"$scripts_directory"/tsui/copy-to-tux.sh
+"$scripts_directory"/tux/build.sh
 
 # replace in desktop 
-    $scripts_directory/desktop/vm-replace-widgets.sh
+"$scripts_directory"/desktop/vm-replace-widgets.sh
--- scripts/workflows/frontend/build-and-update-widget-in-platforms.sh
+++ scripts/workflows/frontend/build-and-update-widget-in-platforms.sh
@@ -7,13 +7,13 @@ scripts_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts/projects"
 widget_name=$1
 
 # build widget in tsui
-$scripts_directory/tsui/build-widget.sh $widget_name
+"$scripts_directory"/tsui/build-widget.sh "$widget_name"
 
 # replace in mobile 
-$scripts_directory/tsui/copy-widget-to-mobile.sh $widget_name
+"$scripts_directory"/tsui/copy-widget-to-mobile.sh "$widget_name"
 
 # replace in tux 
-$scripts_directory/tsui/copy-widget-to-tux.sh $widget_name
+"$scripts_directory"/tsui/copy-widget-to-tux.sh "$widget_name"
 
 # replace in desktop 
-$scripts_directory/tsui/copy-widget-to-desktop.sh $widget_name
+"$scripts_directory"/tsui/copy-widget-to-desktop.sh "$widget_name"
