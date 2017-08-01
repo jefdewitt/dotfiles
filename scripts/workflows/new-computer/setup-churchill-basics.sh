5efbf999b6bc578ddab01de0fc4d463ad56fa6e7
CJ Taylor <cj.taylor@cdinteractive.com>
2017-08-01 07:51:04 -0400

    Added setup-churchill-basics for chat and browsers.

---
+++ scripts/workflows/new-computer/setup-churchill-basics.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# HOW TO USE: https://github.cdinteractive.com/cj-taylor/dotfiles/blob/master/scripts/workflows/new-computer/ReadMe.md
+
+${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT"}
+
+scripts_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts"
+
+declare -a apps=(
+    "flowdock"
+    "joinme"
+    "google-chrome"
+    "mozilla-firefox"
+)
+
+for app in "${apps[@]}"
+do
+    echo "installing $app"
+    sh "$scripts_directory"/apps/"$app"/install.sh
+done
