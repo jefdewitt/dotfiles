2be35637fa1ad9435da7b0faa94e0f67358cd1e6
CJ Taylor <cj.taylor@cdinteractive.com>
2017-08-01 08:17:51 -0400

    Updated bats, docker, go, and zsh.

--- scripts/apps/docker/install.sh
+++ scripts/apps/docker/install.sh
@@ -1,9 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # https://download.docker.com/mac/stable/Docker.dmg
 
-${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT"}
-
-scripts_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts"
-
-$scripts_directory/helpers/install-dmg-from-command-line.sh https://download.docker.com/mac/stable/Docker.dmg docker
+brew cask install docker
