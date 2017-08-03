7b93539329fe858d9c5c0b264325d5a605fab920
CJ Taylor <cj.taylor@cdinteractive.com>
2017-08-03 09:05:47 -0400

    Added oh-my-zsh navigation, syntax-highlighting.

---
+++ scripts/apps/oh-my-zsh-syntax-highlighting/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# https://github.com/mattjj/my-oh-my-zsh/tree/master/plugins/zsh-syntax-highlighting
+cd ~/.oh-my-zsh/custom/plugins
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
