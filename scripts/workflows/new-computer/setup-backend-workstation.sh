4ba5c59d76bda8de4d0cfa455140e4b91f1d8e7b
CJ Taylor <cj.taylor@cdinteractive.com>
2017-08-03 09:20:30 -0400

    Backend workstation - defined an mvp.

---
+++ scripts/workflows/new-computer/setup-backend-workstation.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT"}
+
+projects_directory="$DEV_PROJECTS_ROOT"
+scripts_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts"
+workflows_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts/workflows"
+
+# make our projects directory if it doesn't exist
+mkdir -p "$projects_directory"
+
+"$workflows_directory"/new-computer/setup-churchill-basics.sh
+
+declare -a apps=(
+    "intellij-idea"
+    "java"
+    "postman"
+)
+
+for app in "${apps[@]}"
+do
+    echo "Installing: $app"
+    sh "$scripts_directory"/apps/"$app"/install.sh || {
+        echo "Failed to install: $app"
+    }
+done
