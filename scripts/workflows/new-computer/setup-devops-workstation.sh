3407aee3c82dafa0be48e00cb6b13ce1a0eb17f1
CJ Taylor <cj.taylor@cdinteractive.com>
2017-08-03 09:20:03 -0400

    devops workstation - cleanup.

--- scripts/workflows/new-computer/setup-devops-workstation.sh
+++ scripts/workflows/new-computer/setup-devops-workstation.sh
@@ -1,6 +1,25 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+${DEV_PROJECTS_ROOT:?"Need to set DEV_PROJECTS_ROOT"}
+
+projects_directory="$DEV_PROJECTS_ROOT"
 scripts_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts"
+workflows_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts/workflows"
 
-"$scripts_directory"/apps/sdkman/install.sh
-"$scripts_directory"/apps/docker/install.sh
+# make our projects directory if it doesn't exist
+mkdir -p "$projects_directory"
+
+"$workflows_directory"/new-computer/setup-churchill-basics.sh
+
+declare -a apps=(
+    "sdkman"
+    "docker"
+)
+
+for app in "${apps[@]}"
+do
+    echo "Installing: $app"
+    sh "$scripts_directory"/apps/"$app"/install.sh || {
+        echo "Failed to install: $app"
+    }
+done
