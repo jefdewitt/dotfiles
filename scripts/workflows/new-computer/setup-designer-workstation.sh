e197152184e771966c72e88520385822e0b257c0
CJ Taylor <cj.taylor@cdinteractive.com>
2017-08-04 13:00:13 -0400

    Designer workstation cleanup.

--- scripts/workflows/new-computer/setup-designer-workstation.sh
+++ scripts/workflows/new-computer/setup-designer-workstation.sh
@@ -8,36 +8,35 @@ projects_directory="$DEV_PROJECTS_ROOT"
 scripts_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts"
 workflows_directory="$DEV_PROJECTS_ROOT/dotfiles/scripts/workflows"
 
-# make our projects directory if it doesn't exist 
+# make our projects directory if it doesn't exist
 mkdir -p "$projects_directory"
 
-# install development tooling 
-"$scripts_directory"/apps/homebrew/install.sh
-"$scripts_directory"/apps/intellij-idea/install.sh
-"$scripts_directory"/apps/google-chrome/install.sh
-"$scripts_directory"/apps/mozilla-firefox/install.sh
-"$scripts_directory"/apps/n/install.sh
-"$scripts_directory"/apps/node-version-we-use/install.sh
-"$scripts_directory"/apps/grunt-cli/install.sh
-"$scripts_directory"/apps/xcode-command-line-tools/install.scpt
-"$scripts_directory"/apps/virtualbox/install.sh
-"$scripts_directory"/apps/vagrant/install.sh
+"$workflows_directory"/new-computer/setup-churchill-basics.sh
+"$workflows_directory"/new-computer/setup-frontend-workstation.sh
 
-# install design-team tooling 
-"$scripts_directory"/apps/brackets/install.sh
-"$scripts_directory"/apps/joinme/install.sh
-"$scripts_directory"/apps/boxSync/install.sh
-"$scripts_directory"/apps/filezilla/install.sh
+declare -a apps=(
+    "brackets"
+    "boxSync"
+)
 
-# get the repos 
+declare -a repos=(
+    "twinspires/cdux-mint-julep"
+)
+
+for app in "${apps[@]}"
+do
+    echo "Installing: $app"
+    sh "$scripts_directory"/apps/"$app"/install.sh || {
+        echo "Failed to install: $app"
+    }
+done
+
+# get the repos
 cd "$projects_directory"
-git clone https://github.cdinteractive.com/twinspires/dev_vm.git
-git clone https://github.cdinteractive.com/twinspires/tsui.git
-git clone https://github.cdinteractive.com/twinspires/tux.git
-git clone https://github.cdinteractive.com/twinspires/mobile.git
-git clone https://github.cdinteractive.com/twinspires/whiteLabel.git
-git clone https://github.cdinteractive.com/twinspires/Drupal_Adw.git
-
-# bootstrap the for development 
-"$workflows_directory"/new-computer/setup-host-file.sh
-"$workflows_directory"/new-computer/provision-and-start-vm-for-development.sh
+for repo in "${repos[@]}"
+do
+    echo "Clonnig: $repo"
+    git clone -b production https://github.cdinteractive.com/"$repo".git|| {
+        echo "Failed to clone: $app"
+    }
+done
